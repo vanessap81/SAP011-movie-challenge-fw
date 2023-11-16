e717545948246ef0bf65d79e3220941caba5074d
Vanessa Ponte <vanessaponte@gmail.com>
2023-11-16 11:06:11 -0300

    feat: organização das pastas

--- src/app/components/home/home.module.ts
+++ src/app/components/home/home.module.ts
@@ -4,9 +4,9 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
-import { MainComponent } from './../main/main.component';
-import { HeaderComponent } from './../header/header.component';
-import { FooterComponent } from './../footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
